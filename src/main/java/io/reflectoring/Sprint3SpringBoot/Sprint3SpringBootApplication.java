6aa5f37f01a1adf97f94bc64383e64f352552a9d
Fábio Silva <121696024+FabioTorresSilva@users.noreply.github.com>
2025-02-12 14:52:12 +0000

    Project Init

---
+++ src/main/java/io/reflectoring/Sprint3SpringBoot/Sprint3SpringBootApplication.java
@@ -0,0 +1,13 @@
+package io.reflectoring.Sprint3SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprint3SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprint3SpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/reflectoring/Sprint3SpringBoot/Sprint3SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.reflectoring.Sprint3SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sprint3SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
